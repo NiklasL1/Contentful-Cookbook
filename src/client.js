ef6e3afcf1aee73377785e96348fd0e3f1bd9afd
NiklasL1 <66204186+NiklasL1@users.noreply.github.com>
2020-07-07 17:31:58 +0200

    hiding some tokens

--- src/client.js
+++ src/client.js
@@ -1,6 +1,6 @@
 import * as contentful from 'contentful'
 
 export const client = contentful.createClient ({
-    space: '3k419l8jqovq',
-    accessToken: 'gZ3XSE0HYgnnS270oQ7ASODEL46SHkVB5wz-c4rJsUw'
+    space: process.env.REACT_APP_SPACE_ID,
+    accessToken: process.env.REACT_APP_ACCESS_TOKEN
 })
